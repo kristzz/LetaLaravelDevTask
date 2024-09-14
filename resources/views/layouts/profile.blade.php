d184253eeb236a0ec1b026a72f016d4f41c4fd19
kristzz <emils.krists@gmail.com>
2024-09-15 01:32:36 +0300

    profile page done

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -1,27 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User Profile</title>
-</head>
-<body>
-    <h1>User Profile</h1>
+@extends('layouts.app')
 
-    <p><strong>Name:</strong> {{ $user->name }}</p>
-    <p><strong>Email:</strong> {{ $user->email }}</p>
-    <p><strong>Created At:</strong> {{ $user->created_at->format('Y-m-d H:i:s') }}</p>
-    
-    <h2>My Posts</h2>
-    @if($posts->isEmpty())
-        <p>No posts available.</p>
-    @else
-        <ul>
-            @foreach($posts as $post)
-                <li>
-                    <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-                    <p>{{ $post->content }}</p>
-                </li>
-            @endforeach
-        </ul>
-    @endif
-</body>
-</html>
+@section('content')
+<div class="flex flex-col items-center w-[100vw] mt-8">
+    <h1>User Profile</h1>
+    <div class="flex flex-col items-start">
+        <p>Name: <strong>{{ $user->name }}</strong></p>
+        <p>Email: <strong>{{ $user->email }}</strong></p>
+        <p>Created At: <strong>{{ $user->created_at->format('Y-m-d H:i:s') }}</strong></p>
+    </div>
+    <h2 class="mt-8 text-3xl">My Posts</h2>
+</div>
+@if($posts->isEmpty())
+    <p>No posts available.</p>
+@else
+    @foreach($posts as $post)
+            <x-posts-component :post="$post" />
+    @endforeach
+@endif
+
+@endsection
